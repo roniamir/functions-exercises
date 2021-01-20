6616f59f2513c85dd9fa4d08fbb1efb931ac7a45
roniamir <77384841+roniamir@users.noreply.github.com>
2021-01-20 22:37:23 +0200

    q1 + q2 solutions

--- Exercises.js
+++ Exercises.js
@@ -1,24 +1,45 @@
 //Question 1
 function myReverse(str) {
   // your code here
-  return "";
+  strLen =str.length;
+  let reversStr = str[str.length-1];
+  for( let i = 1 ; i < strLen ; i++){
+    reversStr += str[strLen-1-i];
+  }
+  return reversStr;
 }
 
 //Question 2
 function allCombinations(str) {
   let combinations = [];
+  for(let i = 0; i < str.length ; i++){
+    for(let j = i ; j < str.length ; j++){
+      combinations.push(str.slice(i, j + 1));
+      console.log(str.slice(i, j));
+    }
+  }
   // your code here
 
   //
   return combinations;
 }
+let arr =allCombinations('dog');
+console.log(arr);
 
 //Question 3
 function allCaps(str) {
   // your code here
-  return "";
+  let newStr = str[0].toUpperCase(); 
+  let lastLocation = 1;
+  while(str.indexOf(" ") != -1){
+    newStr += str.slice(lastLocation, str.indexOf(" "));
+    lastLocation = str.indexOf(" ")+1;
+    newStr += str[lastLocation].toUpperCase(); 
+  }
+  return newStr;
 }
-
+let st = "love of my life";
+console.log(allCaps(st));
 //Question 4
 function myPower(x, n) {
   // your code here
