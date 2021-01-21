f2882f14566e89dccf0a7159ad4e52226c9d6173
roniamir <77384841+roniamir@users.noreply.github.com>
2021-01-21 14:09:37 +0200

    final check - all tasts working

--- try.js
+++ try.js
@@ -1,21 +1,18 @@
-function getFirstNotRepeating(str) {
+function allCaps(str) {
   // your code here
-  let  firstNotRepeated = "";
-  let isDup =false;
-  for ( let i = 0; i < str.length ; i++){
-    isDup =false;
-    for ( let j = 0 ; j < str.length && !isDup ; j++ ){
-      if(str[i] === str[j] && i != j){
-        console.log(str[i], str[j]);
-        isDup = true;
-      }
+  let newStr = str[0].toUpperCase();
+  let lastLocation = 0;
+  while(str.indexOf(" ", lastLocation) != -1){
+    if(lastLocation > 0){
+      newStr += str[lastLocation].toUpperCase(); 
     }
-    if(!isDup){
-      firstNotRepeated = str[i];
-      break;
-    }
+    lastLocation++;
+    newStr += str.slice(lastLocation, str.indexOf(" ", lastLocation) + 1);
+    lastLocation = str.indexOf(" ", lastLocation) + 1;
   }
-  return firstNotRepeated;
+   newStr += str[lastLocation].toUpperCase(); 
+    lastLocation++;
+    newStr += str.slice(lastLocation, str.length);
+  return newStr;
 }
-
-console.log(getFirstNotRepeating("abcccfbaefa"));
+console.log(allCaps("i love you"));
